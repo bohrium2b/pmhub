418c60da73b9c930cc74d3811f975f6b5b184edf
Dalton Ma <85321730+bohrium2b@users.noreply.github.com>
2023-07-07 16:45:14 +1200

    Change id to be a UUID field

--- signup/models.py
+++ signup/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
 
@@ -11,6 +12,7 @@ class Performer(models.Model):
     """
     This model defines a performer (user) with an instrument.
     """
+    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     instruments = models.TextField(blank=True)
 
@@ -22,6 +24,7 @@ class Concert(models.Model):
     -----
     This model defines a concert with a dedicated event manager(s), a location, a date and time, a maximum length of time, the availability of piano, description, etc.
     """
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
     title = models.TextField(blank = True, default="Project Melody")
     manager = models.ManyToManyField(User, blank=True)
     location = models.TextField()
@@ -37,6 +40,7 @@ class Performance(models.Model):
     """
     This model defines a performance with a performer(s), a concert,
     """
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
     performer = models.ManyToManyField(User, blank=True)
     concert = models.ForeignKey(Concert, on_delete=models.CASCADE)
     piece = models.TextField()
@@ -44,6 +48,3 @@ class Performance(models.Model):
     arranger = models.TextField(blank=True)
     duration = models.IntegerField()
     comment = models.TextField(blank=True)
-
-
-
