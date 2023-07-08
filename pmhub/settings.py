fc48aa5e1292a3de2527eefaca240172f217485c
Dalton Ma <85321730+bohrium2b@users.noreply.github.com>
2023-07-08 17:40:25 +1200

    Add logging

--- pmhub/settings.py
+++ pmhub/settings.py
@@ -33,7 +33,30 @@ ALLOWED_HOSTS = ["localhost", "*"]
 TIME_ZONE = "Pacific/Auckland"
 LOGIN_URL = "/account/login"
 
-
+LOGGING = {
+    "version": 1,  # the dictConfig format version
+    "disable_existing_loggers": False,  # retain the default loggers
+    "handlers": {
+        "file": {
+            "class": "logging.FileHandler",
+            "filename": "server.log"
+        },
+    },
+    "loggers": {
+        "signup": {
+            "handlers": ["file"],
+            "level": "INFO"
+        },
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO"
+        },
+        "api": {
+            "handlers": ["file"],
+            "level": "INFO"
+        }
+    }
+}
 
 MESSAGE_TAGS = {
         messages.DEBUG: 'alert-secondary',
@@ -144,3 +167,16 @@ CACHES = {
         'LOCATION': 'cache'
     }
 }
+
+REST_FRAMEWORK = {
+    # Use hyperlinked styles by default.
+    # Only used if the `serializer_class` attribute is not set on a view.
+    'DEFAULT_MODEL_SERIALIZER_CLASS':
+        'rest_framework.serializers.HyperlinkedModelSerializer',
+
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+} 
